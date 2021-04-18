ecd4ccebc78684a060240e0a3806916cb890b691
roger_gouveia <roger_gouveia@terceiros.sicredi.com.br>
2021-04-18 14:10:54 -0300

    ajuste nas dependencias e swagger

--- service/app/src/main/java/com/rngouveia/customer/configuration/NettyWebServerConfiguration.java
+++ service/app/src/main/java/com/rngouveia/customer/configuration/NettyWebServerConfiguration.java
@@ -2,10 +2,14 @@ package com.rngouveia.customer.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springdoc.webflux.ui.SwaggerConfig;
+import org.springdoc.webflux.ui.SwaggerWelcomeWebFlux;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.server.reactive.ContextPathCompositeHandler;
 import org.springframework.http.server.reactive.HttpHandler;
 
