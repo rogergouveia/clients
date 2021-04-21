0629b4d0dbe58b96fb1e86515d9c76d496f6decf
roger_gouveia <roger_gouveia@terceiros.sicredi.com.br>
2021-04-21 09:22:41 -0300

    adicionadas configurações do docker

--- service/app/src/main/java/com/rngouveia/customer/domain/Customer.java
+++ service/app/src/main/java/com/rngouveia/customer/domain/Customer.java
@@ -8,10 +8,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
 public class Customer {
-    public enum Status {
-        @JsonProperty("ENABLED") ENABLED,
-        @JsonProperty("DISABLED") DISABLED
-    }
+    public enum Status {ENABLED, DISABLED}
 
     @NotBlank
     private String id;
